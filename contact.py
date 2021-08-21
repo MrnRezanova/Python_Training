8559ced04c84b2c47d4ddb3691620e8f17cc1dbb
Marina <m.rezanova@gemark.ru>
2021-08-21 21:04:11 +0300

    Добавление класса

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, address_1, home_phone,
+                mobile_phone, work_phone, fax, email, email_2, email_3, homepage, b_day, b_month, b_year, ann_day, ann_month,
+                ann_year, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.address_1 = address_1
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.b_day = b_day
+        self.b_month = b_month
+        self.b_year = b_year
+        self.ann_day = ann_day
+        self.ann_month = ann_month
+        self.ann_year = ann_year
+        self.phone2 = phone2
+        self.notes = notes
