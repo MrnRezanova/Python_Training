f7deff98243cc5949cf820f320d18e1d99a00008
Marina <m.rezanova@gemark.ru>
2021-08-21 19:20:42 +0300

    Отдельные параметры для логина и пароля

--- test_add_group.py
+++ test_add_group.py
@@ -7,12 +7,12 @@ def open_home_page(wd):
     wd.get("http://localhost/addressbook/")
 
 
-def login(wd):
+def login(wd, username, password):
     wd.find_element_by_name("user").clear()
-    wd.find_element_by_name("user").send_keys("admin")
+    wd.find_element_by_name("user").send_keys(username)
     wd.find_element_by_name("pass").click()
     wd.find_element_by_name("pass").clear()
-    wd.find_element_by_name("pass").send_keys("secret")
+    wd.find_element_by_name("pass").send_keys(password)
     wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
@@ -53,7 +53,7 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         open_home_page(wd)
-        login(wd)
+        login(wd, username="admin", password="secret")
         open_groups_page(wd)
         create_group(wd)
         return_to_groups_page(wd)
