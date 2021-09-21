c5f9ef5094d8d158722265433107d5ac9dfd4162
Marina <m.rezanova@gemark.ru>
2021-09-21 20:43:05 +0300

    Добавлена проверка телефонов у контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.select import Select
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -8,7 +9,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
+        if not (wd.current_url.endswith("/addressbook/") and len(
+                wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
             wd.find_element_by_link_text("home").click()
 
     def open_add_new_contact_page(self):
@@ -18,8 +20,15 @@ class ContactHelper:
 
     def open_edit_page_by_index(self, index):
         wd = self.app.wd
-        if not (len(wd.find_elements_by_xpath("//div[@id='content']/form/input[22]")) > 0 and len(wd.find_elements_by_xpath("//div[@id='content']/form[2]/input[2]")) > 0):
+        self.app.open_home_page()
+        if not (len(wd.find_elements_by_xpath("//div[@id='content']/form/input[22]")) > 0 and len(
+                wd.find_elements_by_xpath("//div[@id='content']/form[2]/input[2]")) > 0):
             wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
+
+    def open_view_page_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_elements_by_xpath("//img[@alt='Details']")[index].click()
 
     def change_field_select_value(self, field_name, text):
         wd = self.app.wd
@@ -102,8 +111,36 @@ class ContactHelper:
             self.app.open_home_page()
             self.contact_cache = []
             for element in wd.find_elements_by_css_selector("tr")[1:]:
+                cells = element.find_elements_by_tag_name("td")
                 id = element.find_element_by_name("selected[]").get_attribute("value")
-                firstname = element.find_elements_by_css_selector("td")[2].text
-                lastname = element.find_elements_by_css_selector("td")[1].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                firstname = cells[2].text
+                lastname = cells[1].text
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  home_phone=all_phones[0], mobile_phone=all_phones[1],
+                                                  work_phone=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_edit_page_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, id=id, lastname=lastname, home_phone=homephone, work_phone=workphone,
+                       mobile_phone=mobilephone, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_view_page_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone=homephone, work_phone=workphone,
+                       mobile_phone=mobilephone, phone2=phone2)
