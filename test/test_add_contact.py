fa9454711d5a50269a50a43236111bb7302bb1e4
Marina <m.rezanova@gemark.ru>
2021-09-22 16:03:06 +0300

    Добавлена параметризация при создании контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,26 @@
 from model.contact import Contact
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_new_contact(app):
-    contact = Contact(firstname="First name", lastname="Last name")
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="")] + [
+    Contact(firstname=random_string("firstname", 6), lastname=random_string("lastname", 8))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Group.id_or_max) == sorted(new_contacts, key=Group.id_or_max)
-
-
-#def test_add_new_empty_contact(app):
-#    contact = Contact(firstname=" ")
-#   old_contacts = app.contact.get_contact_list()
-#    app.contact.create(contact)
-#   new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
