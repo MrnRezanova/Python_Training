4df80b7b450f44104744bd31873f408fbc16f92e
Marina <m.rezanova@gemark.ru>
2021-10-10 21:18:14 +0300

    Переделка теста на сравнение полей контактов

--- test/test_all_entry_field_contact.py
+++ test/test_all_entry_field_contact.py
@@ -5,14 +5,16 @@ from model.group import Group
 def test_all_entry_field_contact_on_edit_page(app, orm):
     all_contacts_from_db = orm.get_contact_list()
     contacts_from_home_page = app.contact.get_contact_list()
+    sorted_contacts_from_db = sorted(all_contacts_from_db, key=Group.id_or_max)
+    sorted_contacts_from_home_page = sorted(contacts_from_home_page, key=Group.id_or_max)
     for contact in all_contacts_from_db:
         contact.all_email_from_db = merge_emails_like_on_home_page(contact)
         contact.all_phones_from_db = merge_phones_like_on_home_page(contact)
-    for index in range(len(all_contacts_from_db)):
-        assert all_contacts_from_db[index].all_email_from_db == contacts_from_home_page[index].all_email_from_homepage
-        assert all_contacts_from_db[index].all_phones_from_db == contacts_from_home_page[index].all_phones_from_homepage
-        assert all_contacts_from_db[index].address == contacts_from_home_page[index].address
-    assert sorted(all_contacts_from_db, key=Group.id_or_max) == sorted(contacts_from_home_page, key=Group.id_or_max)
+    for index in range(len(sorted_contacts_from_db)):
+        assert sorted_contacts_from_db[index].all_email_from_db == sorted_contacts_from_home_page[index].all_email_from_homepage
+        assert sorted_contacts_from_db[index].all_phones_from_db == sorted_contacts_from_home_page[index].all_phones_from_homepage
+        assert sorted_contacts_from_db[index].address == sorted_contacts_from_home_page[index].address
+    assert sorted_contacts_from_db == sorted_contacts_from_home_page
 
 
 
